9fc042f7d8f3b67fbd85098e7870a871dd0a9aa4
inganta23 <johanes230500@gmail.com>
2023-02-25 09:53:07 +0700

    feat: build frontend

---
+++ prisma/migrations/20230223031553_add_tables/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" SERIAL NOT NULL,
+    "valid" BOOLEAN NOT NULL DEFAULT true,
+    "userAgent" TEXT,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "valid" BOOLEAN NOT NULL DEFAULT true,
+    "content" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import createServer from './utils/server';
+import path from 'path';
+import express, { Request, Response } from 'express';
 
 const PORT = process.env.PORT || 3000;
 const app = createServer();
+
+//---Deployment
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, '/frontend/dist')));
+    app.get('*', (req: Request, res: Response) => {
+        res.sendFile(path.resolve(__dirname1, 'frontend', 'dist', 'index.html'));
+    });
+} else {
+    app.get('/', (req: Request, res: Response) => {
+        res.send('API is Running');
+    });
+}
+
+//---Deployment
 
 app.listen(PORT, () => console.log(`REST API server ready at: http://localhost:${PORT}`));
